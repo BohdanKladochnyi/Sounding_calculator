449b2e7d9c774a3a1f64117e6305221a55021898
Bohdan Kladochnyi <bodya1379@gmail.com>
2024-07-05 18:31:08 +0300

    fix connecting to server check

--- Sounding_calculator/main.cpp
+++ Sounding_calculator/main.cpp
@@ -40,8 +40,10 @@ int main() {
             std::wstring url_addr = input.url;
             std::wstring destFile = input.in_filename;
 
-            while (!S_OK == URLDownloadToFile(NULL, url_addr.c_str(), destFile.c_str(), 0, NULL))
+            while (!(S_OK == URLDownloadToFile(NULL, url_addr.c_str(), destFile.c_str(), 0, NULL))) {
                 std::cerr << "Cannot connect to URL address or invalid date or station number\n";
+                Sleep(3000);
+            }
         
             std::wcout << L"--------------------------------------------------------\n";
             std::wcout << L"Sounding data saved to '" << input.in_filename << L"'\n";
@@ -52,77 +54,77 @@ int main() {
             std::fstream input_soundings;
             input_soundings.open(input.in_filename, std::ios::in);
 
-        if (input_soundings.is_open()) {
-            std::string tp;
-            int current_day = -1;
+            if (input_soundings.is_open()) {
+                std::string tp;
+                int current_day = -1;
 
-            std::vector<PresTempHum> sounding;
-            sounding.reserve(128);
+                std::vector<PresTempHum> sounding;
+                sounding.reserve(128);
 
-            while (std::getline(input_soundings, tp)) {
-                auto pos = tp.find("Observations at ");
-                int hour = -1;
+                while (std::getline(input_soundings, tp)) {
+                    auto pos = tp.find("Observations at ");
+                    int hour = -1;
 
-                //searching for needed observation hour
-                while (hour != station.hour) {
-                    pos = tp.find("Observations at ");
+                    //searching for needed observation hour
+                    while (hour != station.hour) {
+                        pos = tp.find("Observations at ");
 
-                    if (pos != std::string::npos) {
-                        hour = std::stoi(&tp[pos + 16]); //plus size of 'Observations at '
-                        current_day = std::stoi(&tp[pos + 20]);
+                        if (pos != std::string::npos) {
+                            hour = std::stoi(&tp[pos + 16]); //plus size of 'Observations at '
+                            current_day = std::stoi(&tp[pos + 20]);
+                        }
+
+                        std::getline(input_soundings, tp);
+                        if (input_soundings.eof()) break;
+                    }
+                    if (input_soundings.eof()) break;
+
+                    //searching where sounding data begins
+                    while (!is_number_row(tp)) {
+                        std::getline(input_soundings, tp);
+                        if (input_soundings.eof()) break;
+                    }
+                    if (input_soundings.eof()) break;
+
+                    //reads all data from certain sounding
+                    while (is_number_row(tp)) {
+                        std::vector<double> row = string_to_vector(tp);
+                        if (row.size() == 5)
+                            sounding.push_back(filter_row(row));
+                        if (row.size() == 3 && row[1] > 10000.0)
+                            sounding.push_back(filter_short_row(row)); //if humidity is absent
+                        std::getline(input_soundings, tp);
+                        if (input_soundings.eof()) break;
+                    }
+                    if (input_soundings.eof()) break;
+
+                    //check if the upper sounding height is suitable
+                    if (!check_upper_height(sounding.back())) {
+                        std::wcout << L"Upper sounding height is lower then " << minHeight
+                            << L" at day " << current_day << '\n';
+                        sounding.clear();
+                        continue;
                     }
 
-                    std::getline(input_soundings, tp);
-                    if (input_soundings.eof()) break;
-                }
-                if (input_soundings.eof()) break;
+                    //supplement sounding with SMA data
+                    supplement_sounding(sounding);
+                    //interpolate starting point of sounding to GNSS station height
+                    insert_interpolating(sounding, station.height);
 
-                //searching where sounding data begins
-                while (!is_number_row(tp)) {
-                    std::getline(input_soundings, tp);
-                    if (input_soundings.eof()) break;
-                }
-                if (input_soundings.eof()) break;
+                    //calculating tropospheric delay components
+                    tropo_delay.push_back(calculate_components(sounding, station));
+                    tropo_delay.back().day = current_day;
+                    tropo_delay.back().T = sounding[0].T;
 
-                //reads all data from certain sounding
-                while (is_number_row(tp)) {
-                    std::vector<double> row = string_to_vector(tp);
-                    if (row.size() == 5)
-                        sounding.push_back(filter_row(row));
-                    if (row.size() == 3 && row[1] > 10000.0)
-                        sounding.push_back(filter_short_row(row)); //if humidity is absent
-                    std::getline(input_soundings, tp);
-                    if (input_soundings.eof()) break;
-                }
-                if (input_soundings.eof()) break;
-
-                //check if the upper sounding height is suitable
-                if (!check_upper_height(sounding.back())) {
-                    std::wcout << L"Upper sounding height is lower then " << minHeight
-                        << L" at day " << current_day << '\n';
                     sounding.clear();
-                    continue;
                 }
 
-                //supplement sounding with SMA data
-                supplement_sounding(sounding);
-                //interpolate starting point of sounding to GNSS station height
-                insert_interpolating(sounding, station.height);
+                input_soundings.close();
 
-                //calculating tropospheric delay components
-                tropo_delay.push_back(calculate_components(sounding, station));
-                tropo_delay.back().day = current_day;
-                tropo_delay.back().T = sounding[0].T;
-
-                sounding.clear();
+            } else {
+                std::cerr << "Opening file error\n";
+                return -1;
             }
-
-            input_soundings.close();
-
-        } else {
-            std::cerr << "Opening file error\n";
-            return -1;
-        }
 
             //dump in result file
             std::fstream res_out;
@@ -136,7 +138,7 @@ int main() {
                 res_out << "\n";
 
                 std::wcout << L"Result saved to '" << input.out_filename << L"'\n";
-                std::wcout << L"--------------------------------------------------------\n";
+                std::wcout << L"--------------------------" << i << L"/12" << L"--------------------------\n";
 
                 res_out.close();
             }
